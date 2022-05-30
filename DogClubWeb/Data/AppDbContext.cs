1a97f0a0a2a169e32b084e14fd296d5cceb0586d
contihan <daniel.hanak@outlook.cz>
2022-05-30 20:30:02 +0200

    Přidejte soubory projektu.

---
+++ DogClubWeb/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DogClubWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogClubWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Dog>? Dogs { get; set; }
+    }
+}
